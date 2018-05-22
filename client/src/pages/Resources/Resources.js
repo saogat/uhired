cb0a46a9770a18e26b6ff428b4dc8dc53da1226f
Kristen M SPECKHALS <kristenmspeckhals@Kristens-MacBook-Pro.local>
2018-05-22 11:32:45 -0400

    updates to jobs, resources, and portfolio page

--- client/src/pages/Resources/Resources.js
+++ client/src/pages/Resources/Resources.js
@@ -19,7 +19,7 @@ state = {
       </ResourceContainer>
       <h1 style={{textAlign: "center"}}>Resources</h1>
       <hr />
-      <p style={{fontSize: "20px", marginLeft: "30px", marginTop: "30px"}}>Select one or more skills to search.</p>
+      <p style={{fontSize: "20px", marginLeft: "30px", marginTop: "30px"}}>Select one or more technologies to search.</p>
       <Form>
     <ResourceSelection />
     <Button style = {{marginLeft: "20px", marginTop: "10px"}} className = "large blue" type='submit'>Search</Button>
@@ -66,27 +66,27 @@ state = {
        <Table.HeaderCell width={3}>Actions</Table.HeaderCell>
         <Table.HeaderCell width={6}>Resource Search Results</Table.HeaderCell>
         <Table.HeaderCell width={1}>Remove</Table.HeaderCell>
-        <Table.HeaderCell width={6}>Notes</Table.HeaderCell>
+        <Table.HeaderCell width={6}>Description</Table.HeaderCell>
       </Table.Row>
     </Table.Header>
 
     <Table.Body>
       <Table.Row>
-        <Table.Cell><Button className="blue">Add to Portfolio</Button><AddResourceNoteModal />
+        <Table.Cell><Button className="blue">Add to Portfolio</Button>
         </Table.Cell>
         <Table.Cell>Learning HTML</Table.Cell>
         <Table.Cell><Button icon="remove" style={{color: "blue", align: "center"}}  /></Table.Cell>
         <Table.Cell>Great article</Table.Cell>
       </Table.Row>
       <Table.Row>
-      <Table.Cell><Button className="blue">Add to Portfolio</Button><AddResourceNoteModal />
+      <Table.Cell><Button className="blue">Add to Portfolio</Button>
         </Table.Cell>
         <Table.Cell>Semantic UI for Beginners</Table.Cell>
         <Table.Cell><Button icon="remove" style={{color: "blue"}}/></Table.Cell>
         <Table.Cell>......</Table.Cell>
       </Table.Row>
       <Table.Row>
-      <Table.Cell><Button className="blue">Add to Portfolio</Button><AddResourceNoteModal />
+      <Table.Cell><Button className="blue">Add to Portfolio</Button>
         </Table.Cell>
         <Table.Cell>React Tutorial</Table.Cell>
         <Table.Cell><Button icon="remove" style={{color: "blue"}}/></Table.Cell>
