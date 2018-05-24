c02ddb43098e3b5e7279646eacfd13fff0241a6e
Saogat Rab <saogat.rab@icloud.com>
2018-05-24 14:41:30 -0400

    adds static routing

--- server.js
+++ server.js
@@ -9,9 +9,17 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets
-// app.use(express.static("public));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 // Add routes, both API and view
 app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/uhired");
