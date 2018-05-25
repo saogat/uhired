c4cabc88f0f98ba34b6f6d9c77ecd8ee0bb0055a
Saogat Rab <saogat.rab@icloud.com>
2018-05-24 20:27:34 -0400

    changed server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
-import bodyParser from 'body-parser';
-import express from 'express';
-import path from 'path';
-import mongoose from "mongoose";
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-const router = express.Router();
-const staticFiles = express.static(path.join(__dirname, '../../client/build'));
+import bodyParser from 'body-parser'
+import express from 'express'
+import path from 'path'
+const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(staticFiles);
-app.use(router);
+const router = express.Router()
+
+const staticFiles = express.static(path.join(__dirname, '../../client/build'))
+app.use(staticFiles)
+
+app.use(router)
 
 // any routes not picked up by the server api will be handled by the react router
 app.use('/*', staticFiles)
@@ -21,7 +20,3 @@ app.set('port', (process.env.PORT || 3001))
 app.listen(app.get('port'), () => {
     console.log(`Listening on ${app.get('port')}`)
 })
-
-process.env.MONGODB_URI || "mongodb://localhost/uhired");
-
-process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); });
