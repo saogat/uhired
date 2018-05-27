858e1c7b0e315091698779b95e7bc4763a432972
Kristen M SPECKHALS <kristenmspeckhals@Kristens-MacBook-Pro.local>
2018-05-27 18:16:20 -0400

    fixed scrolling on about page

--- client/src/components/Grid/AboutGrid.js
+++ client/src/components/Grid/AboutGrid.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import { Grid, Container, Sticky } from 'semantic-ui-react'
 import MainBreadCrumb from "../BreadCrumb/BreadCrumb.js";
-import FooterDiv from "../../components/Footer/Footer.js";
 import "./AboutUs.css";
 
 const AboutGrid = () => (
     <div>
 
-    <Sticky><Container className= "ui fluid inverted vertical masthead padded left aligned segment massive">
-    <MainBreadCrumb/>
-    </Container></Sticky>
-    <Container className= "ui fluid vertical masthead center aligned segment massive">
-    <h1 style={{fontFamily: "cursive"}}>Meet the Team</h1>
+    <Sticky style={{width: "100%",   position: "absolute", zIndex: "100"}} >
+      <Container style={{width: "100%"}} className= "ui fluid inverted vertical masthead padded left aligned segment massive">
+      <MainBreadCrumb/>
+      </Container>
+    </Sticky>
+    <Container className="ui fluid vertical center aligned segment massive" >
+    <h1/>
     </Container>
-    <Container className="ui fluid vertical masthead center aligned segment massive">
+    <Container  style={{bottomBorder: "2px"}} className="ui fluid vertical center aligned segment massive">
+    <h1 style={{fontFamily: "cursive", marginTop:"40px"}}>Meet the Team</h1>
+    </Container>
     <Grid className= "centered" columns={6} divided>
-    <Grid.Row>
+    <Grid.Row style={{ marginTop: "50px"}}>
       <Grid.Column>
       <img className="ui medium circular image" src="https://s3.amazonaws.com/uhired/CodyG.jpg" alt="Cogy G"/>        
       </Grid.Column>
-      <Grid.Column>
+      <Grid.Column style={{marginRight: "5%"}}>
       <h2 style={{fontFamily: "cursive"}}>Cody Goss</h2>
       <div className="ui bulleted list">
         <div className="item">Full-Stack MERN Developer Enthusiast</div>
@@ -28,7 +31,7 @@ const AboutGrid = () => (
         <div className="item">Tea Sommelier</div>
       </div>
       </Grid.Column>
-      <Grid.Column>
+      <Grid.Column style={{marginRight: "5%"}}>
       <h2 style={{fontFamily: "cursive"}}>Kristen Speckhals</h2>
       <div className="ui bulleted list">
         <div className="item">UI Design Lead</div>
@@ -43,12 +46,12 @@ const AboutGrid = () => (
   
     </Grid.Row>
 
-    <Grid.Row>
+    <Grid.Row style={{marginTop: "50px", bottomBorder: "2px"}}>
       <Grid.Column>
       <img className="ui medium circular image" src="https://s3.amazonaws.com/uhired/saogat.jpg" alt="Saogat R"/>
         <p></p>
       </Grid.Column>
-      <Grid.Column>
+      <Grid.Column style={{marginRight: "5%"}}>
       <h2 style={{fontFamily: "cursive"}}>Saogat Rab</h2>
       <div className="ui bulleted list">
         <div className="item">Project Lead</div>
@@ -57,7 +60,7 @@ const AboutGrid = () => (
         <div className="item">Photographer</div>
       </div>
       </Grid.Column>
-      <Grid.Column>
+      <Grid.Column style={{marginRight: "5%"}}>
       <h2 style={{fontFamily: "cursive"}}>Rani Rab</h2>
       <div class="ui bulleted list">
         <div className="item">UI Designer</div>
@@ -66,15 +69,17 @@ const AboutGrid = () => (
         <div className="item">Linguist</div>
       </div>
       </Grid.Column> 
-      <Grid.Column>
+      <Grid.Column >
         <img className="ui medium circular image" src="https://s3.amazonaws.com/uhired/RaniR.jpg" width="150"  height="150" alt="Rani R"/>
         
       </Grid.Column>
     </Grid.Row>
+  
   </Grid>
-  </Container>
-  <div>
-                <FooterDiv /></div>
+  <Container  style={{bottomBorder: "2px"}} className="ui fluid vertical center aligned segment massive">
+    </Container> <div/>
+
+
   </div>
 )
 
