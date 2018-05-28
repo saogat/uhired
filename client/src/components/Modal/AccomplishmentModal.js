d19edd4252aeead7a825e050617a88a4401b40e3
Saogat Rab <saogat.rab@icloud.com>
2018-05-27 20:01:04 -0400

    minor refactoring

--- client/src/components/Modal/AccomplishmentModal.js
+++ client/src/components/Modal/AccomplishmentModal.js
@@ -5,17 +5,24 @@ import "./Modal.css";
 
 class ModalAccomplishment extends Component {
 
+  //===================================================
+  //Initialize state
   state = { modalOpen: false }
   
+  //===================================================
+  // Modal Functions
     handleOpen = () => {
         this.setState({ modalOpen: true }); 
     };
+    
     handleClose = () => {
         this.setState({ modalOpen: false });
         console.log("Project Added")
-        // this.props.openResource();
+        this.props.loadAccomplishments();
     }
      
+  //===================================================
+  // Render component
     render() {
       return (
         <Modal 
@@ -27,9 +34,8 @@ class ModalAccomplishment extends Component {
           <Image wrapped size='tiny' src='https://st2.depositphotos.com/3265223/11274/v/950/depositphotos_112741124-stock-illustration-handshake-line-icon-deal-partnership.jpg' />
               Add Accomplishment
            </Modal.Header>
-          {/* <Header icon='browser' content='Cookies policy' /> */}
           <Modal.Content>
-          <AccomplishmentForm close={this.handleClose}/>
+              <AccomplishmentForm close={this.handleClose}/>
           </Modal.Content>
         </Modal>
       )
